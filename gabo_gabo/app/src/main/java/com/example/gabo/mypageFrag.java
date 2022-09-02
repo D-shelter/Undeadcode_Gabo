47e6e8fb8bf7608e1f4c42d75757ce345f18ae45
ojy <dsignshelter@naver.com>
2022-09-02 13:56:23 +0900

    ojy add requset

--- gabo_gabo/app/src/main/java/com/example/gabo/mypageFrag.java
+++ gabo_gabo/app/src/main/java/com/example/gabo/mypageFrag.java
@@ -1,18 +1,32 @@
 package com.example.gabo;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.ParseError;
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.HttpHeaderParser;
 import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class mypageFrag extends Fragment {
 
@@ -25,6 +39,10 @@ public class mypageFrag extends Fragment {
     LinearLayout mycomment_open; //게시물 모아보기 버튼
 
 
+    TextView mypage_tv_findnum, mypage_tv_hidenum, mypage_tv_likenum, mypage_tv_level;
+    String hidetrs,findtrs,like,level;
+
+
     MyFindTreasureFrag myFindTreasureFrag; //내가 찾은 보물 바텀시트 프래그먼트
     MyHideTreasureFrag myHideTreasureFrag; //내가 숨긴 보물 바텀시트 프래그먼트
     MyCommentsFrag myCommentsFrag; //게시물 모아보기 바텀시트 프래그먼트
@@ -33,6 +51,9 @@ public class mypageFrag extends Fragment {
 
     private RequestQueue queue;
     private StringRequest stringRequest;
+
+
+
 
     @Nullable
     @Override
@@ -86,8 +107,88 @@ public class mypageFrag extends Fragment {
             }
         });
 
+        sendRequestmyhide();
+
         return view;
 
 
+
     }
+
+
+
+    //내가 숨긴 보물 리퀘스트
+    public void sendRequestmyhide(){
+        // Volley Lib 새로운 요청객체 생성
+        queue = Volley.newRequestQueue(this.getActivity());
+        // 서버에 요청할 주소
+        String url = "http://192.168.21.252:5013/mypage";
+        // 요청 문자열 저장
+        stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            // 응답데이터를 받아오는 곳
+            @Override
+            public void onResponse(String response) {
+                Log.v("resultValue",response);
+                String[] res = response.split(",");
+                String hidetrs = res[0];
+                String findtrs = res[1];
+                String like = res[2];
+                String level = res[3];
+
+                mypage_tv_findnum = getActivity().findViewById(R.id.mypage_tv_findnum);
+                mypage_tv_hidenum = getActivity().findViewById(R.id.mypage_tv_hidenum);
+                mypage_tv_likenum = getActivity().findViewById(R.id.mypage_tv_likenum);
+                mypage_tv_level = getActivity().findViewById(R.id.mypage_tv_level);
+
+                mypage_tv_findnum.setText(hidetrs);
+                mypage_tv_hidenum.setText(findtrs);
+                mypage_tv_likenum.setText(like);
+                mypage_tv_level.setText("Lv"+level);
+
+            }
+        }, new Response.ErrorListener() {
+            // 서버와의 연동 에러시 출력
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        }){
+            @Override //response를 UTF8로 변경해주는 소스코드
+            protected Response<String> parseNetworkResponse(NetworkResponse response) {
+                try {
+                    String utf8String = new String(response.data, "UTF-8");
+                    return Response.success(utf8String, HttpHeaderParser.parseCacheHeaders(response));
+                } catch (UnsupportedEncodingException e) {
+                    // log error
+                    return Response.error(new ParseError(e));
+                } catch (Exception e) {
+                    // log error
+                    return Response.error(new ParseError(e));
+                }
+            }
+            // 보낼 데이터를 저장하는 곳
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+
+                String user_id;
+                user_id = getArguments().getString("user_id");
+
+                params.put("id",user_id);
+
+                return params;
+            }
+        };
+
+
+        //TAG는 필수 아님
+        String TAG = "ojy";
+        stringRequest.setTag(TAG);
+        queue.add(stringRequest);
+
+
+    }
+
+
+
 }
